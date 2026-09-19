fe6ea6440294676109875ec1faefedec75494956
77odk <15527855717@163.com>
2026-09-19 13:04:13 +0800

    feat(ui9): load canonical tokens before legacy styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
+import './styles/tokens.css'
 import './index.css'
 import './styles/ui2.css'
 import './styles/spaceLetter.css'
